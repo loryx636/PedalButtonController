829c232026c4fa3b371bd99f60e9f5b2206027ae
Lorenzo <Lorenzo@FissoLavoro>
2018-09-07 01:00:41 +0200

    prove personalizzazioni

---
+++ build-OSHStudio-Desktop_Qt_5_11_1_MinGW_32bit-Debug/debug/moc_oshpincombobox.cpp
@@ -0,0 +1,154 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'oshpincombobox.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../OSHStudio/oshpincombobox.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'oshpincombobox.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_oshpincombobox_t {
+    QByteArrayData data[9];
+    char stringdata0[126];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_oshpincombobox_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_oshpincombobox_t qt_meta_stringdata_oshpincombobox = {
+    {
+QT_MOC_LITERAL(0, 0, 14), // "oshpincombobox"
+QT_MOC_LITERAL(1, 15, 12), // "item_changed"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 14), // "set_pin_number"
+QT_MOC_LITERAL(4, 44, 7), // "uint8_t"
+QT_MOC_LITERAL(5, 52, 17), // "get_current_index"
+QT_MOC_LITERAL(6, 70, 17), // "set_current_index"
+QT_MOC_LITERAL(7, 88, 31), // "on_comboBox_currentIndexChanged"
+QT_MOC_LITERAL(8, 120, 5) // "index"
+
+    },
+    "oshpincombobox\0item_changed\0\0"
+    "set_pin_number\0uint8_t\0get_current_index\0"
+    "set_current_index\0on_comboBox_currentIndexChanged\0"
+    "index"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_oshpincombobox[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   39,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    1,   40,    2, 0x0a /* Public */,
+       5,    0,   43,    2, 0x0a /* Public */,
+       6,    1,   44,    2, 0x0a /* Public */,
+       7,    1,   47,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 4,    2,
+    0x80000000 | 4,
+    QMetaType::Void, 0x80000000 | 4,    2,
+    QMetaType::Void, QMetaType::Int,    8,
+
+       0        // eod
+};
+
+void oshpincombobox::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        oshpincombobox *_t = static_cast<oshpincombobox *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->item_changed(); break;
+        case 1: _t->set_pin_number((*reinterpret_cast< uint8_t(*)>(_a[1]))); break;
+        case 2: { uint8_t _r = _t->get_current_index();
+            if (_a[0]) *reinterpret_cast< uint8_t*>(_a[0]) = std::move(_r); }  break;
+        case 3: _t->set_current_index((*reinterpret_cast< uint8_t(*)>(_a[1]))); break;
+        case 4: _t->on_comboBox_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (oshpincombobox::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&oshpincombobox::item_changed)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject oshpincombobox::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_oshpincombobox.data,
+      qt_meta_data_oshpincombobox,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *oshpincombobox::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *oshpincombobox::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_oshpincombobox.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int oshpincombobox::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void oshpincombobox::item_changed()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
